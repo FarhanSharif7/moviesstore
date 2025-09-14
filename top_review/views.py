ea5deced753b0afe995379bcd69c32e63629a22d
Sharif, Farhan <fsharif3@gatech.edu>
2025-09-14 01:14:49 -0400

    added return statement

--- top_review/views.py
+++ top_review/views.py
@@ -4,4 +4,5 @@ from movies.models import Review
 def index(request):
     # Get all reviews ordered by likes descending
     top_reviews = Review.objects.select_related('user').order_by('-likes')[:20]
+    return render(request, 'top_review/index.html', {'top_reviews': top_reviews})
     
