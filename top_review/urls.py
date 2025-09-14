caa49b8b4daf92179e403cfd5a945c768d5e9c1c
Sharif, Farhan <fsharif3@gatech.edu>
2025-09-14 01:20:46 -0400

    added ability to like reviews

--- top_review/urls.py
+++ top_review/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='top_review.index'),
+    path('like/<int:review_id>/', views.like_review, name='top_review.like_review'),
 ]
--- top_review/views.py
+++ top_review/views.py
@@ -5,4 +5,14 @@ def index(request):
     # Get all reviews ordered by likes descending
     top_reviews = Review.objects.select_related('user').order_by('-likes')[:20]
     return render(request, 'top_review/index.html', {'top_reviews': top_reviews})
+
+from django.shortcuts import redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
+@require_POST
+def like_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    review.likes += 1
+    review.save()
+    return redirect('top_review.index')
     
