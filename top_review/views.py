aece03e247e3952fb953db3ff44f6bd5d509c8a7
Sharif, Farhan <fsharif3@gatech.edu>
2025-09-14 00:47:02 -0400

    Reviews updated

--- top_review/views.py
+++ top_review/views.py
@@ -1,7 +1,7 @@
-def index(request):
 from django.shortcuts import render
 from movies.models import Review
+
 def index(request):
     # Get all reviews ordered by likes descending
     top_reviews = Review.objects.select_related('user').order_by('-likes')[:20]
-    return render(request, 'top_review/index.html', {'top_reviews': top_reviews})
+    
